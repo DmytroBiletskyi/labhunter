b48b10072999afd079e559b13c848a06fa1a2a48
Dima <babaha3000@gmail.com>
2022-06-06 19:35:56 +0300

    create and customize admin panel delete wrong html filter in base.html

--- labhunter/service/admin.py
+++ labhunter/service/admin.py
@@ -4,4 +4,25 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Files)
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'teacher', 'topic', 'work_number')
+    list_display_links = ('id', 'topic')
+    search_fields = ('teacher', 'topic')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cat_name')
+    list_display_links = ('id', 'cat_name')
+    search_fields = ('name',)
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sub_name', 'course', 'semester')
+    list_display_links = ('id', 'sub_name')
+    search_fields = ('sub_name', 'course', 'semester')
+
+
+admin.site.register(Files, ServiceAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subject, SubjectAdmin)
--- labhunter/service/models.py
+++ labhunter/service/models.py
@@ -1,34 +1,50 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Files(models.Model):
-    teacher = models.CharField(max_length=255)
-    docx = models.FileField(upload_to='files/docx/')
-    topic = models.CharField(max_length=255, null=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', null=True)
-    work_number = models.IntegerField(null=True, blank=True)
-    other_files = models.FileField(upload_to='files/other/', blank=True)
-    cat = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
-    sub = models.ForeignKey('Subject', on_delete=models.CASCADE, null=True, blank=True)
+    teacher = models.CharField(max_length=255, verbose_name='Викладач')
+    docx = models.FileField(upload_to='files/docx/', verbose_name='Шлях до документу')
+    topic = models.CharField(max_length=255, null=True, verbose_name='Тема роботи')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', null=True, verbose_name='Титулка')
+    work_number = models.IntegerField(null=True, blank=True, verbose_name='Номер роботи')
+    other_files = models.FileField(upload_to='files/other/', blank=True, verbose_name='Дадаткові файли')
+    cat = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, verbose_name='Категорія')
+    sub = models.ForeignKey('Subject', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Предмет')
 
     def __str__(self):
         return self.topic
 
+    class Meta:
+        verbose_name = 'Усі файли'
+        verbose_name_plural = 'Усі файли'
+
 
 class Category(models.Model):
-    cat_name = models.CharField(max_length=100, db_index=True)
+    cat_name = models.CharField(max_length=100, db_index=True, verbose_name='Категорія')
 
     def __str__(self):
         return self.cat_name
 
+    def get_absolute_url(self):
+        return reverse('select_cat', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категорії'
+        verbose_name_plural = 'Категорії'
+
 
 class Subject(models.Model):
-    sub_name = models.CharField(max_length=255, db_index=True)
-    course = models.IntegerField()
-    semester = models.IntegerField()
+    sub_name = models.CharField(max_length=255, db_index=True, verbose_name='Назва предмету')
+    course = models.IntegerField(verbose_name='Курс')
+    semester = models.IntegerField(verbose_name='Семестр')
 
     def __str__(self):
         return self.sub_name
+
+    class Meta:
+        verbose_name = 'Предмети'
+        verbose_name_plural = 'Предмети'
 
 
 # from service.models import *
